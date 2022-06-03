4fdfc69acae7793a6c85c0edcf03fb141147d1e3
Your Namefotostoys <ciel2000@gmail.com>
2022-06-03 14:10:49 -0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,42 @@
+gsap.to("#bg", {
+    scrollTrigger: {
+        scrub: 1,
+    },
+    scale: 1.5,
+});
+gsap.to("#triangle", {
+    scrollTrigger: {
+        scrub: 1,
+    },
+    scale: 0.5,
+});
+gsap.to("#left", {
+    scrollTrigger: {
+        scrub: 1,
+    },
+    x: -500,
+});
+gsap.to("#right", {
+    scrollTrigger: {
+        scrub: 1,
+    },
+    x: 500,
+});
+gsap.to("#nube1", {
+    scrollTrigger: {
+        scrub: 1,
+    },
+    x: 200,
+});
+gsap.to("#nube2", {
+    scrollTrigger: {
+        scrub: 1,
+    },
+    x: -200,
+});
+gsap.to("#text", {
+    scrollTrigger: {
+        scrub: 1,
+    },
+    y: 500,
+});
